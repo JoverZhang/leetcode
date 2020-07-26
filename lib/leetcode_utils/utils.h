d712d8d147869e7bd556f0bf728b35676f944db8
JoverZhang <joverzh@gmail.com>
2020-07-26 16:43:45 +0800

    feat(leetcode_utils): add test_tool.
    - add batch_invoke.

--- lib/leetcode_utils/utils.h
+++ lib/leetcode_utils/utils.h
@@ -2,12 +2,18 @@
 #define LEETCODE_UTILS_H
 
 #include <iostream>
+#include <vector>
 #include "./print.h"
+#include "./test_tool.h"
 
 using std::string;
 using std::cin;
 using std::cout;
 using std::endl;
+using std::vector;
+using std::min;
+using std::max;
 using lc_utils::print;
+using lc_utils::batch_invoke;
 
 #endif //LEETCODE_UTILS_H
