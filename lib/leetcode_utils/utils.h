a913ef007e4a553827e83abdd1f259b8aaca13f0
JoverZhang <joverzh@gmail.com>
2020-07-31 22:44:30 +0800

    chore: utils add unordered_map.

--- lib/leetcode_utils/utils.h
+++ lib/leetcode_utils/utils.h
@@ -4,6 +4,7 @@
 #include <cmath>
 #include <climits>
 #include <vector>
+#include <unordered_map>
 #include "./print.h"
 #include "./executor.h"
 
@@ -12,6 +13,7 @@ using std::cin;
 using std::cout;
 using std::endl;
 using std::vector;
+using std::unordered_map;
 using std::min;
 using std::max;
 using std::pow;
