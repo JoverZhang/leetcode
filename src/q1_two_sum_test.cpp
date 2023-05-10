44ef62ea7b0edf9bedd2751659fdf3e9f311ac3a
JoverZhang <joverzh@gmail.com>
2023-05-10 18:16:59 +0800

    test: import gtest

--- src/q1_two_sum_test.cpp
+++ src/q1_two_sum_test.cpp
@@ -1,13 +1,14 @@
 #include <leetcode_utils/utils.h>
+#include <gtest/gtest.h>
 
 class TwoSumBest {
  public:
-  static vector<int> twoSum(vector<int> &nums, int target) {
+  static vector<int> twoSum(vector<int>& nums, int target) {
     unordered_map<int, int> map;
 
-    for (int i = 0; auto num : nums){
+    for (int i = 0; auto num : nums) {
       if (auto search = map.find(target - num); search != map.end()) {
-        return { search->second, i };
+        return {search->second, i};
       }
 
       map.try_emplace(num, i++);
@@ -17,15 +18,12 @@ class TwoSumBest {
   }
 };
 
-int main() {
-  vector v1{ 2, 7, 11, 15 };
+TEST(Q1_TWO_SUM, 1) {
+  vector v1{2, 7, 11, 15};
   auto result1 = TwoSumBest::twoSum(v1, 9);
-
 
   for (auto e : result1) {
     cout << e << ' ';
   }
   cout << endl;
-
-  return 0;
 }
